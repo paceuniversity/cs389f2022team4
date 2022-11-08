b957a2943c5bae7a1b2ab93bc08b7b154539b4fc
Xiaofeng <korate.qwq@gmail.com>
2022-11-07 22:18:10 -0500

    sdk

--- app/src/main/java/com/example/kirinrecipe/HomePage.java
+++ app/src/main/java/com/example/kirinrecipe/HomePage.java
@@ -2,7 +2,9 @@ package com.example.kirinrecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    public void OpenSettingPage(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
