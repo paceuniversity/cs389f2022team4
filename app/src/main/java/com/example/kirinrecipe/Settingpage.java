1a59ccf6d46e1bd80d64f40001b4d11b43f0d52a
Kuan000 <kuan95211@gmail.com>
2022-11-08 22:50:43 -0500

    updating Setting page by Kuan

--- app/src/main/java/com/example/kirinrecipe/Settingpage.java
+++ app/src/main/java/com/example/kirinrecipe/Settingpage.java
@@ -2,7 +2,9 @@ package com.example.kirinrecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settingpage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Settingpage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingpage);
+    }
+
+    public void GotoAbout(View view) {
+        Intent intent=new Intent(Settingpage.this,About.class);
+        startActivity(intent);
     }
 }
