409a2cd96e95b53424a3aa790a91655e2338b636
Kuan000 <kuan95211@gmail.com>
2022-11-20 01:20:09 -0500

    update DIY page by Kuan

--- app/src/main/java/com/example/kirinrecipe/DIYrecipes.java
+++ app/src/main/java/com/example/kirinrecipe/DIYrecipes.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DIYrecipes extends AppCompatActivity {
+public class DIYrecipes extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diyrecipes);
+        //The system progress bar has a relatively large limit and can only set 2 colors
+        findViewProg(R.id.progressbar);
+        setProg(350);
+
+        //Customize the use of the progress bar, you can set 3 colors
+        findViewText();
+
+        //The number of textviews of type, the proportion of weight weight
+        setWeight(1, 2);
+        setWeight(2, 3);
+        setWeight(3, 1);
     }
 }
