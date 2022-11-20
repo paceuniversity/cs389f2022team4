9228d6f559a483d41d45899d34f05ca0f749e77d
zhenshimin <41864758@qq.com>
2022-11-20 18:51:13 -0500

    DIY2

---
+++ app/src/main/java/com/example/kirinrecipe/diyrecipes_2.java
@@ -0,0 +1,14 @@
+package com.example.kirinrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class diyrecipes_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diyrecipes2);
+    }
+}
