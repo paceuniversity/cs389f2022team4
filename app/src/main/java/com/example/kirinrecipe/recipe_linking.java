bafa33a673fb9ccd3a550cc694a32b565298a557
Kuan000 <kuan95211@gmail.com>
2022-11-19 22:58:11 -0500

    creat Recipe Linking page by Kuan

---
+++ app/src/main/java/com/example/kirinrecipe/recipe_linking.java
@@ -0,0 +1,26 @@
+package com.example.kirinrecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recipe_linking extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_linking);
+
+        //The system progress bar has a relatively large limit and can only set 2 colors
+        findViewProg(R.id.progressbar);
+        setProg(350);
+
+        //Customize the use of the progress bar, you can set 3 colors
+        findViewText();
+
+        //The number of textviews of type, the proportion of weight weight
+        setWeight(1, 2);
+        setWeight(2, 3);
+        setWeight(3, 1);
+    }
+}
